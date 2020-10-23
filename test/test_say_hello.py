a0482834d39e2309b873b86b234a5d995cd3c885
Nguyễn Nhĩ Thái <62824044+nhithai4620@users.noreply.github.com>
2020-10-23 11:36:56 +0700

    thai commit

--- test/test_say_hello.py
+++ test/test_say_hello.py
@@ -1,11 +1,8 @@
-# -*- coding: utf-8 -*-
 import blank
-
 
 def test_say_hello():
     try:
         blank.main.say_hello()
         assert True
-    except Exception as e:
-        print(e)
-        assert False
+    except:
+        assert False    
